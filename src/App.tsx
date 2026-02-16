233b9465e116eb3789334ad786c3b01a066bf272
Polina <polina.nikolskaia@gmail.com>
2026-02-16 11:00:04 +0300

    remove unused react-router-dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,10 @@
-import { BrowserRouter } from 'react-router-dom';
 import { ThemeProvider } from './context/ThemeContext';
 import { Converter } from './components/Converter/Converter';
 
 function App() {
   return (
     <ThemeProvider>
-      <BrowserRouter basename="/currency-converter">
-        <Converter />
-      </BrowserRouter>
+      <Converter />
     </ThemeProvider>
   );
 }
